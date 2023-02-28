b1c0aa6c71dbb57304690c400cbdba78c9d9e376
Olha Yarosh <yarosholha1@gmail.com>
2023-03-01 00:27:34 +0200

    Added info to footer part.

--- src/PageBox/MainPage/Footer/Footer.js
+++ src/PageBox/MainPage/Footer/Footer.js
@@ -4,12 +4,27 @@ import classes from './Footer.module.css';
 const Footer = () => {
     return (
         <div className={classes.footer}>
-            <hr/>
+            <hr className={classes.footerUnderline}/>
             <div className={classes.addressTitle}>
                 Адреса:
             </div>
+            <hr className={classes.addressUnderline}/>
             <div className={classes.address}>
-                Церква Покрови Пресвятої Богородиці<br/>вул. Дрогобицька, 12/а<br/>м. Трускавець, Львівська обл.<br/>82200 Україна
+                вул. Дрогобицька, 12/a,<br/>м. Трускавець, Львівська область,<br/>82200 Україна
+            </div>
+            <div className={classes.jurisdictionTitle}>
+                Юрисдикція:
+            </div>
+            <hr className={classes.jurisdictionUnderline}/>
+            <div className={classes.jurisdiction}>
+                Львівська митрополія,<br/>Самбірсько-Дрогобицька єпархія
+            </div>
+            <div className={classes.phoneTitle}>
+                Телефон та E-mail:
+            </div>
+            <hr className={classes.phoneUnderline}/>
+            <div className={classes.phone}>
+                (097) 502 1104,<br/>info@info
             </div>
             <img className={classes.imgEparchy} src='Eparchy.png' alt=''/>
         </div>
